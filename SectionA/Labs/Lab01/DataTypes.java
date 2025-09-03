06c8a97264d6c58107a2158f86ee23aea884d5bb
mshahidbhatti <phdfellows20@gmail.com>
2025-09-03 16:59:10 +0500

    primitve types

---
+++ SectionA/Labs/Lab01/DataTypes.java
@@ -0,0 +1,56 @@
+public class DataTypes{
+
+	public static void main(String a[]){
+		System.out.println("Basic types in java:");
+		
+		long l=100;
+		int i=10; 
+		short s=50;
+		byte b=50;
+			
+		double d=10.0;
+		float f=20.0f;
+
+		char c='c';
+
+		boolean b1=true;   //false
+
+
+		System.out.println("Print string and cursor moves to new line");
+		System.out.print("Print and cursor remains on the same line");
+
+		System.out.printf("\n Information %d",20);
+
+		//System.out.err("Error log");
+
+		System.out.printf("\nValue in int %d",i);
+		System.out.printf("\nValue in long %d",l);
+		System.out.printf("\nValue in short %d",s);
+		System.out.printf("\nValue in long %d",b);
+
+		System.out.printf("\nValue in double %.2f",d);
+		System.out.printf("\nValue in float %.2f",f);
+
+		System.out.printf("\nValue in char %c",c);
+
+		System.out.printf("\nValue in boolean %b",b1);
+
+
+		
+
+		System.out.printf("\n %-50s : %s","Shahid", "Muhammad");
+
+
+
+
+
+
+		
+
+
+
+		
+
+	}
+
+}
